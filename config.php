90797c3adb6f1cca11ae6ccf58b90c7a4b2f833d
dvegas1 <33332525+dvegas1@users.noreply.github.com>
2018-05-12 18:35:41 -0400

    Update config.php

--- config.php
+++ config.php
@@ -3,9 +3,9 @@
 $username="dvegas";
 $password="Tebca2045+";
 $postboxtextsize="1";
-$devicestablerefreshspeed="5";
-$filestablerefreshspeed="5";
-$messageboxrefreshspeed="5";
+$devicestablerefreshspeed=5 * 1000;
+$filestablerefreshspeed=5 * 1000;
+$messageboxrefreshspeed=5 * 1000;
 $offlineminutes="5";
 $timezonesetting="GMT+08:45";
 $autoscrolltextbox="yes";
