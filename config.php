7bb87a21946b9e95d39b94b48de4bc86c719af4f
darwin <darwinvegas1@gmail.com>
2018-05-12 16:58:38 -0400

    pruebasinformacion111

---
+++ config.php
@@ -0,0 +1,13 @@
+<?php
+
+$username="dvegas";
+$password="Tebca2045+";
+$postboxtextsize="1";
+$devicestablerefreshspeed="5";
+$filestablerefreshspeed="5";
+$messageboxrefreshspeed="5";
+$offlineminutes="5";
+$timezonesetting="GMT+08:45";
+$autoscrolltextbox="yes";
+
+?>
