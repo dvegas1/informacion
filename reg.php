be5ee533a27c9e77812b63d93403ec19c55ff792
dvegas1 <33332525+dvegas1@users.noreply.github.com>
2018-05-12 15:38:30 -0400

    Update reg.php

--- reg.php
+++ reg.php
@@ -1,9 +1,7 @@
 <?php
 $allowedDomains = array("informacion.herokuapp.com", "https://informacion.herokuapp.com");
 
-if (in_array($_SERVER['HTTP_HOST'], $allowedDomains)) {
+
 	$validDomain = "true";
-} else {
-	$validDomain = "false";
-}
+
 ?>
