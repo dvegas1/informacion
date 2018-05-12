a78fbc5f23b4fa2c1cb7bac14f1ba60a68f4d861
darwin <darwinvegas1@gmail.com>
2018-05-12 16:16:19 -0400

    pruebas4

--- pruebas/conexion.php
+++ pruebas/conexion.php
@@ -1,5 +1,4 @@
 <?php
-$fff="";
 
 $dbconn = pg_connect("host=ec2-54-163-240-54.compute-1.amazonaws.com dbname=d97hqd0uu802gp user=vxoxyrownzuzgx password=0669e05248eea11fe4bdc6d93f82de2248a0dd9a9345ee09215f6f4d2d393862")
     or die('No se ha podido conectar: ' . pg_last_error());
