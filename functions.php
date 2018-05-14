1a8bb75388b6dd28da36df9fd5c69ee044f4f876
darwin <darwinvegas1@gmail.com>
2018-05-14 09:53:25 -0400

    pruebasinformacionultimo1405201811

--- functions.php
+++ functions.php
@@ -8,14 +8,13 @@
   }
   
     //$connect = new PDO("mysql:host=$dbhost;dbname=$dbname", $dbuser, $dbpass);
-	$connect = pg_connect("host=ec2-54-163-240-54.compute-1.amazonaws.com dbname=d97hqd0uu802gp user=vxoxyrownzuzgx password=0669e05248eea11fe4bdc6d93f82de2248a0dd9a9345ee09215f6f4d2d393862")
-    or die('No se ha podido conectar: ' or die('La consulta fallo: ' . pg_last_error());
 
 
-
-
-
-
+  $host="ec2-54-163-240-54.compute-1.amazonaws.com";
+  $dbname="d97hqd0uu802gp";
+  $dbuser="vxoxyrownzuzgx";
+  $dbpass="0669e05248eea11fe4bdc6d93f82de2248a0dd9a9345ee09215f6f4d2d393862";
+$connect = pg_connect($host $dbname $dbuser $dbpass) or die('No se ha podido conectar: ' . pg_last_error());
 
 
 
