16c7f1ec0b2f1533c70b3165c71a6f926ecf3f0c
darwin <darwinvegas1@gmail.com>
2018-05-14 10:47:56 -0400

    pruebasinformacionultimo140520181111

--- functions.php
+++ functions.php
@@ -8,24 +8,15 @@
   }
   
     //$connect = new PDO("mysql:host=$dbhost;dbname=$dbname", $dbuser, $dbpass);
-<<<<<<< HEAD
-=======
-	$connect = pg_connect("host=ec2-54-163-240-54.compute-1.amazonaws.com dbname=d97hqd0uu802gp user=vxoxyrownzuzgx password=0669e05248eea11fe4bdc6d93f82de2248a0dd9a9345ee09215f6f4d2d393862") or die ('No se ha podido conectar: ' or die('La consulta fallo: ' . pg_last_error()));
 
 
-
-
->>>>>>> ff1e221ca8585bc399cc0c80c8737d45551f3242
-
-
-  $host="ec2-54-163-240-54.compute-1.amazonaws.com";
-  $dbname="d97hqd0uu802gp";
-  $dbuser="vxoxyrownzuzgx";
-  $dbpass="0669e05248eea11fe4bdc6d93f82de2248a0dd9a9345ee09215f6f4d2d393862";
-$connect = pg_connect($host $dbname $dbuser $dbpass) or die('No se ha podido conectar: ' . pg_last_error());
-
-
-
+    //$connect = new PDO("mysql:host=$dbhost;dbname=$dbname", $dbuser, $dbpass);
+$connect = pg_connect("host=ec2-54-163-240-54.compute-1.amazonaws.com port=5432 dbname=d97hqd0uu802gp user=vxoxyrownzuzgx password=0669e05248eea11fe4bdc6d93f82de2248a0dd9a9345ee09215f6f4d2d393862") or die('No se ha podido conectar: ' . pg_last_error());;
+//conectarse a una base de datos llamada "mary" en el host "sheep" con el nombre de usuario y password
+if (!$connect) {
+  echo "Ocurrió un error.\n";
+  exit;
+}
 
   
 
@@ -51,7 +42,7 @@ function setFunction($UID, $Function){
 		$functions = getFunction($UID);
 		if($functions == ""){
 
-			$statement="UPDATE slaves SET Function='$Function' WHERE 'Unique_ID' = '$UID'") or die('La consulta fallo: ' . pg_last_error());
+			$statement=("UPDATE slaves SET Function='$Function' WHERE 'Unique_ID' = '$UID'") or die('La consulta fallo: ' . pg_last_error());
     
 			$statement1=pg_query($statement) or die('La consulta fallo: ' . pg_last_error());
 
