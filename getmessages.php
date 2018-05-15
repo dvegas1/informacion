e1add39f2447f3ab1fdaa33aa5e574c35cd24c01
darwin <darwinvegas1@gmail.com>
2018-05-15 16:52:13 -0400

    ultimo1

--- getmessages.php
+++ getmessages.php
@@ -6,15 +6,22 @@ if($_GET['uid']!=""){
   
   $mycommand = "SELECT * FROM messages WHERE uid='$getuid'";
   
+
   foreach ($connect->query($mycommand) as $row) {
     $spacedmessage = str_replace("*", " ", $row['message']);
 	echo $spacedmessage;
 	echo "<br/>";
   }
 } else {
+
 	$mycommand = "SELECT * FROM messages";
 	
-	foreach ($connect->query($mycommand) as $row) {
+	//$mycommand1 = pg_query($mycommand) or die('La consulta fallo: ' . pg_last_error());
+
+
+//$result=pg_query($connect,$mycommand);
+
+	foreach (pg_query($connect,$mycommand) as $row) {
 	  $spacedmessage = str_replace("*", " ", $row['message']);
 	  echo "<strong>" . $row['uid'] . ":</strong> ";
       echo $spacedmessage;
